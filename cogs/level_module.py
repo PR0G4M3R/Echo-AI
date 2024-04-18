183de14f9301dc8169df7cd17739d08cbb246df2
PR0G4M3R <118217184+PR0G4M3R@users.noreply.github.com>
2024-04-17 17:13:30 -0700

    1.245

--- cogs/level_module.py
+++ cogs/level_module.py
@@ -200,6 +200,7 @@ class levelModule(commands.Cog):
     @commands.command()
     async def view_level(self, ctx, user: discord.Member = None):
         user = user or ctx.author
+        guild_id = ctx.guild.id
         level = await self.get_level(user.id)
         await ctx.send(f"{user.display_name} is at level {level}.")
 
